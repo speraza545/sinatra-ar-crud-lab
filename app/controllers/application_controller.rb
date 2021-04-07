fcfb32af08173dc7c35e09b2ed5370516932fb4a
speraza545 <s.peraza545@gmail.com>
2021-04-07 16:46:42 -0400

    commit tree c53fcc33d4cb576fc87a346177483cf5f76bd283

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,40 @@ class ApplicationController < Sinatra::Base
 
   get '/' do
   end
+
+  get '/articles' do 
+    @articles = Article.all
+    erb :index
+  end
+
+  get '/articles/new' do 
+    erb :new
+  end
+
+  get "/articles/:id" do 
+    @article = Article.find(params[:id])
+    erb :show
+  end
+
+  post "/articles" do 
+    @new_article = Article.new(params)
+    @new_article.save
+    redirect "/articles/#{@new_article.id}"
+  end
+
+  get '/articles/:id/edit' do
+    @article = Article.find(params[:id])
+    erb :update
+  end
+
+  patch "/articles/:id" do 
+    puts params
+    Article.update(params[:id], title: params[:title], content: params[:content])
+    redirect "articles/#{params[:id]}"
+  end
+
+  delete '/articles/:id' do 
+    Article.destroy(params[:id])
+    redirect to "/articles"
+  end
 end
